7049cc21f3496802333d8137b0fe71c65bac4e53
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2025-12-15 21:11:35 +0100

    signed urls to use authorization tokens

---
+++ src/helpers/urls.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.SCRAPER_SECRET;
+
+/**
+ * Generate a signed URL with a JWT token appended as query parameter.
+ * Exprires after 10 minutes.
+ * @param {string} baseUrl
+ * @returns {string}
+ */
+export function generateSignedURL(baseUrl) {
+
+	const token = jwt.sign(
+		{
+			url: baseUrl,
+			exp: Math.floor(Date.now() / 1000) + 60 * 10
+		},
+		SECRET
+	);
+
+	const url = new URL(baseUrl);
+	url.searchParams.append("token", token);
+	return url.toString();
+
+};
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,64 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+const SECRET = process.env.SCRAPER_SECRET;
 const API_KEY = process.env.SCRAPER_API_KEY;
 
 function authMiddleware(req, res, next) {
+    const { token: queryToken, url } = req.query;
+
+    console.log('TOKEN', queryToken);
+
+    if (req.url.endsWith('.m3u8')) {
+        console.log('Playlist request', queryToken);
+    } else {
+        console.log('Segment request', queryToken);
+    }
+
+    if (queryToken) {
+        try {
+            const payload = jwt.verify(queryToken, SECRET);
+
+            const protocol =
+                req.headers['x-forwarded-proto'] || req.protocol || 'http';
+            const host = req.headers.host;
+            const pathname = req.path;
+
+            const queryParams = new URLSearchParams();
+            for (const [key, value] of Object.entries(req.query)) {
+                if (key !== 'token') {
+                    queryParams.append(key, value);
+                }
+            }
+
+            const reconstructedUrl = `${protocol}://${host}${pathname}?${queryParams.toString()}`;
+
+            if (payload.url !== reconstructedUrl) {
+                console.log('URL mismatch:');
+                console.log('Expected:', payload.url);
+                console.log('Got:', reconstructedUrl);
+
+                return res.status(403).json({
+                    error: 'Forbidden',
+                    message: 'Token not valid for this URL'
+                });
+            };
+
+            return next();
+        } catch (err) {
+            return res.status(403).json({
+                error: 'Forbidden',
+                message: 'Invalid or expired token'
+            });
+        }
+    }
+
     const authHeader = req.headers.authorization || '';
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({
             error: 'Unauthorized',
-            message: 'Missing or invalid token'
+            message: 'Missing or invalid Authorization header'
         });
     }
 
